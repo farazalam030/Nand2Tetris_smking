76a3f81861697404ebd58bf99777ff39ea227a01
Faraz Alam <faraz@mac.in.ibm.com>
2025-07-04 12:31:11 +0530

    cleanup

--- 11/JackAnalyzer/Parser.py
+++ 11/JackAnalyzer/Parser.py
@@ -12,7 +12,7 @@ from JackConstant import *
 class ParserError(Exception):
     def __init__(self, message):
         self.message = message
-    
+
 class Parser(object):
     def __init__(self, file):
         self.lex = Lex(file)
@@ -21,8 +21,8 @@ class Parser(object):
         self.openout(file)
         self.compile_class()
         self.closeout()
-        
-    # VMWriter support    
+
+    # VMWriter support
     def openout(self, path):
         outdir = os.path.join(os.path.dirname(path), 'output')
         file = os.path.join(outdir, os.path.basename(path))
@@ -31,20 +31,20 @@ class Parser(object):
         except OSError as e:
             pass
         self.vm.openout(file)
-    
+
     def closeout(self):
         self.vm.closeout()
 
     def vm_function_name(self):
         return self._cur_class+'.'+self._cur_subroutine
-        
+
     def vm_push_variable(self, name):
         (type, kind, index) = self.symbols.lookup(name)
         self.vm.write_push(segments[kind], index)
-        
+
     def vm_pop_variable(self, name):
         (type, kind, index) = self.symbols.lookup(name)
-        self.vm.write_pop(segments[kind], index)     
+        self.vm.write_pop(segments[kind], index)
 
     # Routines to advance the token
     def _require(self, tok, val=None):
@@ -53,28 +53,28 @@ class Parser(object):
             raise ParserError(self._require_failed_msg(tok, val))
         else:
             return lexval
-            
+
     def _require_failed_msg(self, tok, val):
         if val == None: val = tokens[tok]
         return 'Expected '+val
-    
+
     def _advance(self):
         return self.lex.advance()
 
     def _is_token(self, tok, val=None):
         lextok, lexval = self.lex.peek()
         return val == None and lextok == tok or (lextok, lexval) == (tok, val)
-    
+
     def _is_keyword(self, *keywords):
         lextok, lexval = self.lex.peek()
         return lextok == T_KEYWORD and lexval in keywords
-        
+
     def _is_sym(self, symbols):
         lextok, lexval = self.lex.peek()
         return lextok == T_SYM and lexval in symbols
-        
-    # Parser and compile Jack code    
-    # class: 'class' className '{' classVarDec* subroutineDec* '}'    
+
+    # Parser and compile Jack code
+    # class: 'class' className '{' classVarDec* subroutineDec* '}'
     def compile_class(self):
         self._require(T_KEYWORD, KW_CLASS)
         self.compile_class_name()
@@ -88,16 +88,16 @@ class Parser(object):
     # className: identifier
     def compile_class_name(self):
         self._cur_class = self.compile_var_name()   # Class names don't have to go into the symbol table
-        
+
     # Variable declarations
     def _is_class_var_dec(self):
         return self._is_keyword(KW_STATIC, KW_FIELD)
-            
+
     # classVarDec: {'static'|'field'} type varName (',' varName)* ';'
     def compile_class_var_dec(self):
         tok, kwd = self._advance()      # static | field
         self._compile_dec(kwd_to_kind[kwd])
-        
+
     # type varName (',' varName)* ';'
     def _compile_dec(self, kind):
         type = self.compile_type();
@@ -108,7 +108,7 @@ class Parser(object):
             name = self.compile_var_name()
             self.symbols.define(name, type, kind)
         self._require(T_SYM, ';')
-    
+
     def _is_type(self):
         return self._is_token(T_ID) or self._is_keyword(KW_INT, KW_CHAR, KW_BOOLEAN)
 
@@ -119,25 +119,25 @@ class Parser(object):
         else:
             return self.compile_type()
 
-    # type: 'int' | 'char' | 'boolean' | className           
+    # type: 'int' | 'char' | 'boolean' | className
     def compile_type(self):
-        if self._is_type(): 
+        if self._is_type():
             return self._advance()[1]
-        else:               
+        else:
             raise ParserError(self._require_failed_msg(*self.lex.peek()))
-        
+
     def _is_var_name(self):
         return self._is_token(T_ID)
-        
+
     # varName: identifier
     def compile_var_name(self):
         return self._require(T_ID)
-        
+
     # Subroutine declarations
     def _is_subroutine(self):
         return self._is_keyword(KW_CONSTRUCTOR, KW_FUNCTION, KW_METHOD)
-        
-    # subroutineDec: ('constructor'|'function'|'method') ('void'|type) 
+
+    # subroutineDec: ('constructor'|'function'|'method') ('void'|type)
     #                subroutineName '(' parameterList ')' subroutineBody
     def compile_subroutine(self):
         tok, kwd = self._advance()
@@ -150,11 +150,11 @@ class Parser(object):
         self.compile_parameter_list()
         self._require(T_SYM, ')')
         self.compile_subroutine_body(kwd)
-                
+
     # subroutineName: identifier
     def compile_subroutine_name(self):
         self._cur_subroutine = self.compile_var_name()  # subroutine names don't have to go in the symbol table
-        
+
     # parameterList: (parameter (',' parameter)*)?
     def compile_parameter_list(self):
         if self._is_type():
@@ -162,14 +162,14 @@ class Parser(object):
             while self._is_sym(','):
                 self._advance()
                 self.compile_parameter()
-            
-    # parameter: type varName  
+
+    # parameter: type varName
     def compile_parameter(self):
         if self._is_type():
             type = self.compile_type()
             name = self.compile_var_name()
             self.symbols.define(name, type, SK_ARG)
-            
+
     # subroutineBody: '{' varDec* statements '}'
     def compile_subroutine_body(self, kwd):
         self._require(T_SYM, '{')
@@ -182,7 +182,7 @@ class Parser(object):
     def write_func_decl(self, kwd):
         self.vm.write_function(self.vm_function_name(), self.symbols.var_count(SK_VAR))
         self.load_this_ptr(kwd)
-        
+
     def load_this_ptr(self, kwd):
         if kwd == KW_METHOD:
             self.vm.push_arg(0)
@@ -191,24 +191,24 @@ class Parser(object):
             self.vm.push_const(self.symbols.var_count(SK_FIELD))    # object size
             self.vm.write_call('Memory.alloc', 1)
             self.vm.pop_this_ptr()      # set up 'this' pointer to point to new object
-        
+
     def _is_var_dec(self):
         return self._is_keyword(KW_VAR)
-        
+
     # varDec: 'var' type varName (',' varName)* ';'
     def compile_var_dec(self):
         self._require(T_KEYWORD, KW_VAR)
         return self._compile_dec(SK_VAR)
-        
+
     # Statements
     # statement: statement*
     def compile_statements(self):
         while self._is_statement():
             self._compile_statement()
-    
+
     def _is_statement(self):
         return self._is_let() or self._is_if() or self._is_while() or self._is_do() or self._is_return()
-        
+
     # statement: letStatement | ifStatement | whileStatement | doStatement | returnStatement
     def _compile_statement(self):
         if   self._is_let():    self.compile_let()
@@ -216,10 +216,10 @@ class Parser(object):
         elif self._is_while():  self.compile_while()
         elif self._is_do():     self.compile_do()
         elif self._is_return(): self.compile_return()
-        
+
     def _is_let(self):
         return self._is_keyword(KW_LET)
-        
+
     # letStatement: 'let' varName ('[' expression ']')? '=' expression ';'
     def compile_let(self):
         self._require(T_KEYWORD, KW_LET)
@@ -251,7 +251,7 @@ class Parser(object):
 
     def _is_if(self):
         return self._is_keyword(KW_IF)
-        
+
     # ifStatement: 'if' '(' expression ')' '{' statements '}'
     #              ('else' '{' statements '}')?
     def compile_if(self):
@@ -267,7 +267,7 @@ class Parser(object):
 
     def _is_while(self):
         return self._is_keyword(KW_WHILE)
-        
+
     # whileStatement: 'while' '(' expression ')' '{' statements '}'
     def compile_while(self):
         self._require(T_KEYWORD, KW_WHILE)
@@ -293,7 +293,7 @@ class Parser(object):
     def new_label(self):
         self.label_num += 1
         return 'label'+str(self.label_num)
-        
+
     def _is_do(self):
         return self._is_keyword(KW_DO)
 
@@ -304,10 +304,10 @@ class Parser(object):
         self.compile_subroutine_call(name)  # VM code for subroutine call
         self.vm.pop_temp(TEMP_RETURN)       # Pop return value and discard
         self._require(T_SYM, ';')
-        
+
     def _is_return(self):
         return self._is_keyword(KW_RETURN)
-        
+
     # returnStatement: 'return' expression? ';'
     def compile_return(self):
         self._require(T_KEYWORD, KW_RETURN)
@@ -317,7 +317,7 @@ class Parser(object):
             self.vm.push_const(0)       # push 0 if not returning a value
         self._require(T_SYM, ';')
         self.vm.write_return()          # return
-        
+
     # Expressions
     # expression: term (op term)*
     def compile_expression(self):
@@ -327,10 +327,10 @@ class Parser(object):
             op = self._advance()
             self.compile_term()
             self.vm.write_vm_cmd(vm_cmds[op[1]])  # op
-        
+
     def _is_term(self):
         return self._is_const() or self._is_var_name() or self._is_sym('(') or self._is_unary_op()
-        
+
     # term: integerConstant | stringConstant | keywordConstant | varName
     #     | varName '[' expression ']' | subroutineCall | '(' expression ')'
     #     | unaryOp term
@@ -356,16 +356,16 @@ class Parser(object):
 
     def _is_const(self):
         return self._is_token(T_NUM) or self._is_token(T_STR) or self._is_keyword_constant()
-        
+
     def _is_keyword_constant(self):
         return self._is_keyword(KW_TRUE, KW_FALSE, KW_NULL, KW_THIS)
 
     def _is_op(self):
-        return self._is_sym('+-*/&|<>=')    
+        return self._is_sym('+-*/&|<>=')
 
     def _is_unary_op(self):
         return self._is_sym('-~')
-        
+
     # integerConstant | stringConstant | keywordConstant
     def compile_const(self):
         tok, val = self._advance()
@@ -375,14 +375,14 @@ class Parser(object):
             self.write_string_const_init(val)       # initialize string & push str addr
         elif tok == T_KEYWORD:
             self.compile_kwd_const(val)             # push TRUE, FALSE, NULL etc.
-    
+
     def write_string_const_init(self, val):
         self.vm.push_const(len(val))
         self.vm.write_call('String.new', 1)         # String.new(len(str))
         for c in val:
             self.vm.push_const(ord(c))
             self.vm.write_call('String.appendChar', 2)  # String.appendChar(nextchar)
-            
+
     # keywordConstant: 'true' | 'false' | 'null' | 'this'
     def compile_kwd_const(self, kwd):
         if kwd == KW_THIS:
@@ -392,7 +392,7 @@ class Parser(object):
             self.vm.write_vm_cmd('neg')
         else:   # KW_FALSE or KW_NULL
             self.vm.push_const(0)
-    
+
     # '[' expression ']'
     def compile_array_subscript(self, name):
         self.vm_push_variable(name)     # push array ptr onto stack
@@ -429,13 +429,13 @@ class Parser(object):
             name = obj_name+'.'+name
         else:                               # Calling using object variable name
             num_args = 1
-            self.vm_push_variable(obj_name) # push object ptr onto stack   
+            self.vm_push_variable(obj_name) # push object ptr onto stack
             name = self.symbols.type_of(obj_name)+'.'+name
         return num_args, name
-                    
+
     def _is_builtin_type(self, type):
         return type in [KW_INT, KW_CHAR, KW_BOOLEAN, KW_VOID]
-        
+
     # expressionList: (expression (',' expression)*)?
     def compile_expr_list(self):
         num_args = 0
